680b210351924e8be0227221c9ecffb641054fad
Nick Reinicke <Nicholas.Reinicke@nrel.gov>
2026-04-24 11:03:34 -0600

    exp15: HGBR LR 0.1->0.05 + max_iter 100->1000 (paired)

--- train.py
+++ train.py
@@ -53,7 +53,12 @@ CONFIG = POWERTRAINS[POWERTRAIN]
 
 
 def make_model():
-    return HistGradientBoostingRegressor(max_leaf_nodes=255, random_state=52)
+    return HistGradientBoostingRegressor(
+        max_leaf_nodes=255,
+        learning_rate=0.05,
+        max_iter=1000,
+        random_state=52,
+    )
 
 
 def train_model() -> dict:
