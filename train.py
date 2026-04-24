8bf0d7d414e09fc5c35d3f0833fdd8442a78ac2a
Nick Reinicke <Nicholas.Reinicke@nrel.gov>
2026-04-24 11:06:15 -0600

    exp18: LGBM n_estimators 100 -> 500

--- train.py
+++ train.py
@@ -53,7 +53,9 @@ CONFIG = POWERTRAINS[POWERTRAIN]
 
 
 def make_model():
-    return LGBMRegressor(num_leaves=255, random_state=52, verbose=-1)
+    return LGBMRegressor(
+        num_leaves=255, n_estimators=500, random_state=52, verbose=-1
+    )
 
 
 def train_model() -> dict:
