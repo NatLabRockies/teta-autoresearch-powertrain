82d6d0ece7082d4bc2f3bd649d0f4d74cbf2ac1f
Nick Reinicke <Nicholas.Reinicke@nrel.gov>
2026-04-24 11:00:54 -0600

    exp12: HGBR learning_rate 0.1 -> 0.05

--- train.py
+++ train.py
@@ -53,7 +53,7 @@ CONFIG = POWERTRAINS[POWERTRAIN]
 
 
 def make_model():
-    return HistGradientBoostingRegressor(random_state=52)
+    return HistGradientBoostingRegressor(learning_rate=0.05, random_state=52)
 
 
 def train_model() -> dict:
