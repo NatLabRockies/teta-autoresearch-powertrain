34e609fd6baf0776003060fa05e2b23228d36bc9
Nick Reinicke <Nicholas.Reinicke@nrel.gov>
2026-04-24 11:39:40 -0600

    exp36: LGBM n_estimators 500 -> 1000 (with min_child_samples=50)

--- train.py
+++ train.py
@@ -66,7 +66,7 @@ CONFIG = POWERTRAINS[POWERTRAIN]
 def make_model():
     return LGBMRegressor(
         num_leaves=255,
-        n_estimators=500,
+        n_estimators=1000,
         min_child_samples=50,
         random_state=52,
         verbose=-1,
