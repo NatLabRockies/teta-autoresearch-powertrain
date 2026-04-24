de7a5d1c4e45666eb8b3f50c533ed3a46eb8823a
Nick Reinicke <Nicholas.Reinicke@nrel.gov>
2026-04-24 11:37:19 -0600

    exp35: LGBM min_child_samples 20 -> 50

--- train.py
+++ train.py
@@ -65,7 +65,11 @@ CONFIG = POWERTRAINS[POWERTRAIN]
 
 def make_model():
     return LGBMRegressor(
-        num_leaves=255, n_estimators=500, random_state=52, verbose=-1
+        num_leaves=255,
+        n_estimators=500,
+        min_child_samples=50,
+        random_state=52,
+        verbose=-1,
     )
 
 
