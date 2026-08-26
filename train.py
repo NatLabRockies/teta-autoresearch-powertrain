aa40eb2c724417c6ac5024f01160a5b4fe79a604
Nick Reinicke <Nicholas.Reinicke@nrel.gov>
2026-08-25 20:54:02 -0600

    exp38: widen the two-block sequence member 64 -> 128 channels

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ SEQUENCE_FEATURES = [f for f in LINK_FEATURES if not f.startswith("journey_")]
 TARGET = "energy_rate_gge"
 
 # --- sequence member ---
-CHANNELS = 64
+CHANNELS = 128
 DILATIONS = (1, 2)
 KERNEL = 5
 SEQ_LR = 3e-3
