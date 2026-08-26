691aa621cf524f17d1a5e152406e8ab5aa56a691
Nick Reinicke <Nicholas.Reinicke@nrel.gov>
2026-08-25 20:18:23 -0600

    exp34: narrow the sequence member 96 -> 64 channels

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ SEQUENCE_FEATURES = [f for f in LINK_FEATURES if not f.startswith("journey_")]
 TARGET = "energy_rate_gge"
 
 # --- sequence member ---
-CHANNELS = 96
+CHANNELS = 64
 DILATIONS = (1, 2, 4, 8)
 KERNEL = 5
 SEQ_LR = 3e-3
